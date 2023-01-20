3190be6bd3510084814d087c45c996218d7bc614
Amreen <amreen@sixberries.com>
2023-01-20 15:57:28 +0530

    delete-webhook-done-sq-shopify

--- src/lib/jobs/cronjobs/create.js
+++ src/lib/jobs/cronjobs/create.js
@@ -1,10 +1,9 @@
 
 
 const createJob = async (agenda) => {
-  // runs everyday at 12 am
-
-  await agenda.every("0 0 * * *", "update_product_sq");
-  await agenda.every("0 0 * * *", "delete_product_sq");
+  
+  await agenda.every("0 0 * * *", "update_product_sq"); // runs everyday at 12 am
+  await agenda.every("0 2 * * *", "delete_product_sq"); // runs everyday at 2 am
 
 };
 
