936b1f34a8c6d5f10c2fa511dc66ca55c28eefe6
Ankur <ankur@sixberries.com>
2023-04-05 14:45:11 +0530

    Add logs on update order webhook

--- src/services/logger.service.js
+++ src/services/logger.service.js
@@ -1,10 +1,26 @@
 const Logger = require("../models/logger.model");
-
+const LogsModel = require("../models/logs.model")
 
 const createLogger = async (logBody) => {
   return Logger.create(logBody);
 };
 
+const insertLog = async (venderPlatformOrderId, body, logs ) => {
+  try {
+    const updateObj = {
+      venderPlatformOrderId,
+      webHookData: body.webHookData,
+      $push: {logs: logs },
+      logType: body.logType,
+      status: body.status
+    };
+    await LogsModel.update({ venderPlatformOrderId: venderPlatformOrderId }, updateObj, { upsert: true });
+  } catch (error) {
+    console.log("🚀 ~ file: logger.service.js:20 ~ insertLog ~ error:", error)
+  }
+};
+
 module.exports = {
-  createLogger
+  createLogger,
+  insertLog
 }
