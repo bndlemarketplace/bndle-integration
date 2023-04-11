97a94279b087efa561c98729c24e09da8101cfbc
Ankur <ankur@sixberries.com>
2023-04-11 18:27:38 +0530

    resolved xml issue

--- src/lib/jobs/cronjobs/generateXMLMerchantFile.js
+++ src/lib/jobs/cronjobs/generateXMLMerchantFile.js
@@ -142,7 +142,7 @@ module.exports = async (agenda) => {
             xml += `<g:availability>in_stock</g:availability>`;
             xml += `<g:price>${product?.variants[0]?.price} GBP</g:price>`;
             xml += `<g:gtin></g:gtin>`;
-            xml += `<g:brand>${product.vendorName}</g:brand>`;
+            xml += `<g:brand>${encode(product.vendorName, { level: 'xml' })}</g:brand>`;
             xml += "<g:age_group>newborn</g:age_group>"
             xml += "<g:gender>unisex</g:gender>"
             xml += `<g:color>Black/White/Grey/Green/Blue/Pink</g:color>`
