1e4afd992c802560a8b0865eaac1fc635e0f5233
Ankur <ankur@sixberries.com>
2023-04-14 13:42:52 +0530

    Removed logs

--- src/lib/jobs/cronjobs/udpateProductSquarespace.js
+++ src/lib/jobs/cronjobs/udpateProductSquarespace.js
@@ -2,16 +2,6 @@ const { updateAllVendorProducts, deleteVendorProducts } = require('../../../serv
 const logger = require('../../../config/logger');
 
 module.exports = async (agenda) => {
-  agenda.define('update_product_sq', {
-    concurrency: 4, lockLifetime: 1 * 60 * 1000, priority: 1,
-  }, async (job) => {
-    try {
-      console.log('called create squarespace cron')
-      updateAllVendorProducts();
-    } catch (err) {
-      logger.info('Error while running CRON for initializing token : ', err);
-    }
-  });
 
   agenda.define('delete_product_sq', {
     concurrency: 4, lockLifetime: 1 * 60 * 1000, priority: 1,
