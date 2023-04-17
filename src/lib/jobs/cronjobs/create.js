46e96bbbe64df9c6ce04fe6bb6545ebe154bfa4b
Ankur <ankur@sixberries.com>
2023-04-17 18:39:27 +0530

    comment sync serveice

--- src/lib/jobs/cronjobs/create.js
+++ src/lib/jobs/cronjobs/create.js
@@ -7,12 +7,12 @@ const createJob = async (agenda) => {
   await agenda.every("0 0 * * *", "generate_xml_facebook_file");
   
 
-  await agenda.every("0 0 * * *", "update_product_wc"); // runs everyday at 12 am
-  await agenda.every("0 0 * * *", "update_product_wix"); // runs everyday at 12 am
-  await agenda.every("0 0 * * *", "update_product_squareSpace"); // runs everyday at 12 am
+  // await agenda.every("0 0 * * *", "update_product_wc"); // runs everyday at 12 am
+  // await agenda.every("0 0 * * *", "update_product_wix"); // runs everyday at 12 am
+  // await agenda.every("0 0 * * *", "update_product_squareSpace"); // runs everyday at 12 am
 
-  await agenda.every("0 2 * * *", "delete_product_sq"); // runs everyday at 2 am
-  await agenda.every("0 0 * * *", "sync_product_shopify"); // runs everyday at 1 am
+  // await agenda.every("0 2 * * *", "delete_product_sq"); // runs everyday at 2 am
+  // await agenda.every("0 0 * * *", "sync_product_shopify"); // runs everyday at 1 am
 
 };
 
