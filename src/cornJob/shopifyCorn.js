b6841ac8defdbec6e2a5e84a2d7da6221b1ae7c3
Ankur <ankur@sixberries.com>
2023-05-26 16:11:51 +0530

    image issue

--- src/cornJob/shopifyCorn.js
+++ src/cornJob/shopifyCorn.js
@@ -1025,7 +1025,7 @@ const createUpdateProduct = async (product, mode, userId) => {
       product.images.forEach(async (img) => {
         if (img.variant_ids.length === 0) {
           // const Products3url = await s3upload.downloadImgAndUploadToS3(img.src);
-          let oldImg = currentDbProduct.images.findIndex((i) => i.bndleImageId === img.bndleImageId);
+          let oldImg = currentDbProduct.images.findIndex((i) => i.bndleImageId === img.id);
           let mappedImagesIndex = mappedImages.findIndex((i) => i.src === img.src);
           if(mappedImagesIndex === -1) {
             const imgObj = {
