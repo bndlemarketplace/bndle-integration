3cc68f5cf68543a61d56ac142fad4b0fc2a9cace
Ankur <ankur@sixberries.com>
2023-08-03 20:37:39 +0530

    comment category

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -197,7 +197,7 @@ router.route('/generate').get(async (req, res) => {
           xml += `<g:identifier_exists>no</g:identifier_exists>`;
         }
         xml += `<g:product_type>${encode(product.productCategory, { level: 'xml' })}</g:product_type>`
-        xml += `<g:google_product_category>${product?.category} &gt; ${encode(product.subCategory, { level: 'xml' })}</g:google_product_category>`
+        // xml += `<g:google_product_category>${product?.category} &gt; ${encode(product.subCategory, { level: 'xml' })}</g:google_product_category>`
         xml += `<g:shipping>`
         xml += `<g:country>GB</g:country>`
         xml += `<g:service>Standard</g:service>`
